6dad346f1d2e5f39a879f4ece0b59708d854b98f
Rimpal-Patel <rimpal@7span.com>
2022-05-05 19:08:52 +0530

    Committed for collections program

--- src/collectionOperation/ArrayListOperation.java
+++ src/collectionOperation/ArrayListOperation.java
@@ -1,21 +1,37 @@
 package collectionOperation;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 import compare.SortBySalary;
 import model.Employee;
 import Data.EmployeeData;
 
 public class ArrayListOperation {
     public static void main(String[] args) {
+        Employee e5=new Employee(5, "meera",29, " z@gmail.com ", "Female" , "IT", 15000);
         List<Employee> empList = EmployeeData.empData();
         List<Employee> empListCopy = new ArrayList<>();
         empListCopy.addAll(empList);
         Collections.sort(empListCopy, new SortBySalary());
 
-        for (Employee emp: empListCopy) {
+       /* for (Employee emp: empListCopy) {
              System.out.println(emp);
+        }*/
+
+        List<Employee> listCopy = new CopyOnWriteArrayList(empListCopy);
+        Iterator<Employee> listIterator = listCopy.iterator();
+
+        while (listIterator.hasNext())
+        {
+
+            System.out.println(listIterator.next());
+            listCopy.add(e5);
         }
+        System.out.println();
+
     }
 
 }
